1ff4e675d3c944d958d1b5127e2b5e05870e555d
lebarrichello <leticia.barrichello@gmail.com>
2023-08-18 17:39:35 -0300

    :sparkles: feat: Inserindo registros na tabela order_details

--- desafio18.sql
+++ desafio18.sql
@@ -1,3 +1,2 @@
-SELECT COUNT(*) AS orders_count
-FROM orders
-WHERE employee_id IN (5, 6) AND shipper_id = 2;WHERE supplier_id = 3 AND status_id = 2;
+SELECT * FROM purchase_orders
+WHERE supplier_id = 3 AND status_id = 2;
