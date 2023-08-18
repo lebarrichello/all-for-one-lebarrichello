7c75abd16a0feff6bb29b4e3dde4fcb27cb2ac00
lebarrichello <leticia.barrichello@gmail.com>
2023-08-18 17:22:43 -0300

    :sparkles: feat:Mostre a quantidade de pedidos que foram feitos na tabela orders pelo employee_id igual a 5 ou 6, e que foram enviados através do método(coluna) shipper_id igual a 2

--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,3 @@
-SELECT * FROM purchase_orders
-WHERE supplier_id = 3 AND status_id = 2;
+SELECT COUNT(*) AS orders_count
+FROM orders
+WHERE employee_id IN (5, 6) AND shipper_id = 2;WHERE supplier_id = 3 AND status_id = 2;
